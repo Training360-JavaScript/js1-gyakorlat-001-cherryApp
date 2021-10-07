1c258116709a70c91652b4187fcc42ae2f414618
Joe Cserko <j.cserko@gmail.com>
2021-10-07 11:15:40 +0200

    fix

--- js/sum.js
+++ js/sum.js
@@ -2,6 +2,6 @@ const summation = (a = 0, b = 0) => {
     return a + b;
 };
 
-// const subtraction = (a = 0, b = 0) => {
-//     return a - b;
-// };
+const subtraction = (a = 0, b = 0) => {
+    return a - b;
+};
